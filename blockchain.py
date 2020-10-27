345aab2cb8ca0edb4062e5db0858e1343d06928d
sarthak choudhary <f20190112@pilani.bits-pilani.ac.in>
2020-10-27 15:15:38 +0530

    mine block function added

--- blockchain.py
+++ blockchain.py
@@ -59,3 +59,25 @@ class Blockchain:
             block_index += 1
 
         return True
+
+#Part 2 - Mining our Blockchain
+
+#Creating a Web App
+app = Flask(__name__)
+
+blockchain = Blockchain()
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congrats, you just mined a block', 
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+
+    return jsonify(response), 200
